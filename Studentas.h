0f1946fc8edc2626861ee5449d6a60ebc9a4d0ce
Gustelo1 <140171498+Gustelo1@users.noreply.github.com>
2024-01-03 01:39:15 +0200

    Update Studentas.h

--- Studentas.h
+++ Studentas.h
@@ -5,22 +5,28 @@
 #include "mediana.h"
 #include <sstream>
 #include <algorithm>
-#include "Studentas.h"
 
 
-
-class Studentas {
-private:
+class Zmogus {
+protected:
   std::string vardas_;
   std::string pavarde_;
+  Zmogus() : vardas_(""), pavarde_("") {}
+
+public:
+  std::string vardas() const  { return vardas_; }  
+  inline std::string pavarde() const  { return pavarde_; }  
+};
+
+class Studentas : public Zmogus {
+private:
   double egzaminas_;
   std::vector<double> nd_;
+
 
 public:
   Studentas() : egzaminas_(0) { }  // default konstruktorius
   Studentas(std::istream& is);
-  inline std::string vardas() const { return vardas_; }    // get'eriai, inline
-  inline std::string pavarde() const { return pavarde_; }  // get'eriai, inline
   double galBalas(double (*f) (std::vector<double>) = mediana) const;  // get'eriai
   std::istream& readStudent(std::istream&);
 };
