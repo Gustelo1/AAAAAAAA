88d5b070db2013bd381204ed0f19e322b2900d20
Gustelo1 <140171498+Gustelo1@users.noreply.github.com>
2024-01-03 01:22:58 +0200

    header

---
+++ Studentas.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "mediana.h"
+#include <sstream>
+#include <algorithm>
+#include "Studentas.h"
+
+
+
+class Studentas {
+private:
+  std::string vardas_;
+  std::string pavarde_;
+  double egzaminas_;
+  std::vector<double> nd_;
+
+public:
+  Studentas() : egzaminas_(0) { }  // default konstruktorius
+  Studentas(std::istream& is);
+  inline std::string vardas() const { return vardas_; }    // get'eriai, inline
+  inline std::string pavarde() const { return pavarde_; }  // get'eriai, inline
+  double galBalas(double (*f) (std::vector<double>) = mediana) const;  // get'eriai
+  std::istream& readStudent(std::istream&);
+};
