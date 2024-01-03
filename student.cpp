242bb900df8bcecfe00d57cd7657ac429431772f
Gustelo1 <140171498+Gustelo1@users.noreply.github.com>
2024-01-03 19:14:41 +0200

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -14,6 +14,8 @@ double Studentas::galBalas(double (*f) (std::vector<double>)) const {
   return (*f)(visi);
 }
 
+
+
 std::istream& Studentas::readStudent(std::istream& is) {
   std::string line;
   std::getline(is, line);
@@ -30,44 +32,32 @@ std::istream& Studentas::readStudent(std::istream& is) {
 
 
 int main() {
-  std::ifstream file("students_1000000.txt"); // Open the file
-  if (!file.is_open()) {
-    std::cerr << "Unable to open file students.txt" << std::endl;
-    return 1;
-  }
-
-  auto start_reading = std::chrono::high_resolution_clock::now();
-
-  std::vector<Studentas> studentai;
-  std::vector<Studentas> good_students;
-  std::string line;
-  while (std::getline(file, line)) {
-    std::istringstream iss(line);
-    Studentas s(iss);
-    if (s.galBalas() > 5) {
-      good_students.push_back(s);
-    } else {
-      studentai.push_back(s);
+    std::ifstream file("students_1000000.txt"); // Open the file
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file students.txt" << std::endl;
+        return 1;
     }
-  }
 
-  auto end_reading = std::chrono::high_resolution_clock::now();
-  auto duration_reading = std::chrono::duration_cast<std::chrono::milliseconds>(end_reading - start_reading);
+    std::vector<Studentas> studentai;
+    std::vector<Studentas> good_students;
+    std::string line;
+    
+    auto start_sorting = std::chrono::high_resolution_clock::now();
 
-  std::cout << "Reading time: " << duration_reading.count() << " milliseconds" << std::endl;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        Studentas s(iss);
+        if (s.galBalas() > 5) {
+            good_students.push_back(s);
+        } else {
+            studentai.push_back(s);
+        }
+    }
 
-  file.close(); 
+    auto end_sorting = std::chrono::high_resolution_clock::now();
+    auto duration_sorting = std::chrono::duration_cast<std::chrono::milliseconds>(end_sorting - start_sorting);
+    std::cout << "Reading and sorting time: " << duration_sorting.count() << " milliseconds" << std::endl;
 
-
-  auto start_sorting = std::chrono::high_resolution_clock::now();
-  std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
-    return a.galBalas() > b.galBalas(); 
-  });
-  auto end_sorting = std::chrono::high_resolution_clock::now();
-  auto duration_sorting = std::chrono::duration_cast<std::chrono::milliseconds>(end_sorting - start_sorting);
-
-  std::cout << "Sorting time: " << duration_sorting.count() << " milliseconds" << std::endl;
-
-
-  return 0;
+    file.close(); 
+    return 0;
 }
